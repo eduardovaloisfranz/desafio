7569ee66362aab7bcfebf2e120d4b562e17ffc6b
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2024-11-07 18:26:04 -0300

    create topbar

--- desafio-senior/src/Shared/topbar/topbar.component.ts
+++ desafio-senior/src/Shared/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
-
+  @Input() title = ''
 }
